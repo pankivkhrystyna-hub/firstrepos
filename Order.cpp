3fdff0bda49858f210ebf156a5f731afebb3efcc
Khrystyna <pankiv.khrystyna07@gmail.com>
2026-03-02 19:15:01 +0200

    Fix class Order

--- Order.cpp
+++ Order.cpp
@@ -2,21 +2,12 @@
 #include <iostream>
 
 Order::Order(int id, std::string name, double totalamount, std::string status, std::string paymentmethod)
-  : id(id), name(name) , totalamount(totalamount), status(status), paymentmethod(paymentmethod) {
-    std::cout << "order:"<<std::endl;
-    std::cout <<"id: "<< id << std::endl;
-    std::cout <<"name: " << name << std::endl;
-    std::cout << "totalamout: " << totalamount <<std::endl;
-    std::cout <<"status: " << status << std::endl;
-    std::cout <<"payment method: " << paymentmethod <<std::endl;
-}
+  : id(id), name(name) , totalamount(totalamount), status(status), paymentmethod(paymentmethod) {}
 
 Order::Order(int id)
-    :Order(id, std::string(""), 0.0, std::string(""), std::string("")){};
+    :Order(id, std::string(""), 0.0, std::string(""), std::string("")){}
 
-Order::~Order() {
-    std::cout << "order:"<<std::endl;
-}
+Order::~Order() {}
 
 void Order::display() {
     std::cout << "order:"<<std::endl<<"id: "<< id << std::endl<<"name: " << name << std::endl<< "totalamout: " << totalamount <<std::endl<<"status: " << status << std::endl<<"payment method: " << paymentmethod <<std::endl;

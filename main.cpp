74480214fb2581128ccbf0bd2f8c45e8589425e9
Khrystyna <pankiv.khrystyna07@gmail.com>
2026-03-02 10:06:30 +0200

    Add class Order

--- main.cpp
+++ main.cpp
@@ -1,4 +1,39 @@
 #include <iostream>
+using namespace std;
+
+class Order {
+private:
+    int id;
+    string name;
+    double totalamount;
+    string status;
+    string paymentmethod;
+public:
+    //Перевантаження конструктора
+    Order(int id, string name, double total, string status, string payment) : id(id), name(name), totalamount(total), status(status), paymentmethod(payment) {}
+
+    Order(){}
+
+    Order (int i) {
+        id = i;
+        name = "";
+        totalamount = 0;
+        status = "";
+        paymentmethod = "";
+    }
+
+    void print() {
+        cout << "order:"<<endl<<
+            "id: " << id << endl
+        << "name: " << name << endl
+        << "totalamout: " << totalamount<<endl
+        <<"status: " << status << endl
+        <<"payment method: " << paymentmethod <<endl;
+    }
+
+    ~Order() {} //деструктор
+};
+
 
 int main() {
     return 0;

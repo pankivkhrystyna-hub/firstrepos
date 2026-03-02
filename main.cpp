ad08d3640b1f5f33217cdd55cf30ea64863a1a06
Khrystyna <pankiv.khrystyna07@gmail.com>
2026-03-02 20:43:27 +0200

    Initialize Order, Customer and Product in main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "Customer.h"
+#include "Product.h"
+#include "Order.h"
 using namespace std;
 
 int main() {
+    Order order1(1232,"Order Iphone", 16700, "Processing", "Card");
+    order1.display();
+    Customer customer1("Andrey", "andrey@email.com", "+3800954636");
+    customer1.display();
+    Product product1;
+    product1.display();
 
+    return 0;
 }
